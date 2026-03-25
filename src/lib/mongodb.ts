79b41710a1aea29fc26b9324e6577297ef0f6c83
Priya Yadav <your-email@gmail.com>
2026-03-25 18:48:07 +0530

    fixed mongodb connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,16 +1,23 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI!;
-const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
-
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error("Please add MONGODB_URI");
 }
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
